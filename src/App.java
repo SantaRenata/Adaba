a7dee8ba6a2d0da3822f26849932138bd52115f8
SantaRenata <renatasanta2002@gmail.com>
2022-09-19 09:41:20 +0200

    SQLite

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         
-        Data data = new Data(new Mariadb());
+        Data data = new Data(new Sqlite());
         
         //data.connect();
         //itt kérek adatokat
